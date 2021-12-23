7168cc59743f9fef23d789fb6f9a25ffa8bfc163
wwwjjdk <goshasavtch@yandex.ru>
2021-12-23 03:32:49 +0300

    add all

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Stream <Integer> stream = intList.stream()
+             .filter(value -> value>0)
+             .filter(value -> value%2 ==0)
+             .sorted(Comparator.naturalOrder());
+        stream.forEach(x-> System.out.println(x + ";"));
+
+
+
+
+
+
+
+    }
+}
